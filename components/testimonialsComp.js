8f8c103247ee8c49ab3e76d5fcae60f5737ebfb6
mjmorrison10 <86391005+mjmorrison10@users.noreply.github.com>
2022-04-03 14:24:02 -0700

    update testimonial comps

--- components/testimonialsComp.js
+++ components/testimonialsComp.js
@@ -15,6 +15,9 @@ import {
   getStartedBtn,
 } from "../public/Settings/baseSettings";
 import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
   Avatar,
   Grow,
   Pagination,
@@ -23,7 +26,7 @@ import {
   useMediaQuery,
 } from "@mui/material";
 import Skelebar from "./SkeleBar";
-
+import { ExpandMore } from "@mui/icons-material";
 
 function stringToColor(string) {
   let hash = 0;
@@ -60,6 +63,18 @@ export default function TestimonialsComp(props) {
   const matchesMd = useMediaQuery((theme) => theme.breakpoints.up("md"));
   const matchesLg = useMediaQuery((theme) => theme.breakpoints.up("lg"));
   const matchesXl = useMediaQuery((theme) => theme.breakpoints.up("xl"));
+
+  const [expanded, setExpanded] = React.useState(false);
+  const [expandedPanel, setExpandedPanel] = React.useState("");
+
+  const handleExpandedChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+    setExpandedPanel(panel);
+
+    // console.log(expandedPanel);
+  };
+
+  React.useEffect(() => !expanded && setExpandedPanel(""));
 
   const [displayText, setDisplayText] = React.useState(false);
   const [specificCardToDisplay, setSpecificCardToDisplay] =
@@ -104,72 +119,72 @@ export default function TestimonialsComp(props) {
     // setDisplayText(displayText ? panel : false);
   };
 
-  function testimonial() {
-    return (
-      <Paper
-        // onClick={() => {
-        //   console.log(i);
-        //   setDisplayText(!displayText);
-        // }}
-        onClick={handleChange(i)}
-        key={i}
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          padding: 1,
-          gap: 1,
-          width: `${!matchesSm ? "82.5vw" : null}`,
-        }}
-      >
-        <Box
-          display={"flex"}
-          justifyContent={"center"}
-          gap={1}
-          alignItems={"center"}
-        >
-          <Avatar {...stringAvatar(`${list.name}`)} />
-          <Typography>{list.name}</Typography>
-        </Box>
-        <Skelebar h={2} w={"100%"} my={0} clr={randomColorWithAccent} />
+  // function testimonial() {
+  //   return (
+  //     <Paper
+  //       // onClick={() => {
+  //       //   console.log(i);
+  //       //   setDisplayText(!displayText);
+  //       // }}
+  //       onClick={handleChange(i)}
+  //       key={i}
+  //       sx={{
+  //         display: "flex",
+  //         flexDirection: "column",
+  //         padding: 1,
+  //         gap: 1,
+  //         width: `${!matchesSm ? "82.5vw" : null}`,
+  //       }}
+  //     >
+  //       <Box
+  //         display={"flex"}
+  //         justifyContent={"center"}
+  //         gap={1}
+  //         alignItems={"center"}
+  //       >
+  //         <Avatar {...stringAvatar(`${list.name}`)} />
+  //         <Typography>{list.name}</Typography>
+  //       </Box>
+  //       <Skelebar h={2} w={"100%"} my={0} clr={randomColorWithAccent} />
 
-        <Box
-          display={"flex"}
-          flexDirection={"column"}
-          justifyContent={"space-between"}
-          flexGrow={1}
-        >
-          <Typography
-            variant={"body"}
-            noWrap={
-              matchesMd || (!displayText && specificCardToDisplay === i)
-                ? false
-                : true
-            }
-            maxWidth={"65ch"}
-          >
-            {list.testimonial}
-          </Typography>
-          <Box
-            alignSelf={"flex-end"}
-            display={"flex"}
-            alignItems={"center"}
-            justifyContent={"center"}
-          >
-            <Typography variant={"caption"}>Rating:</Typography>
-            <Rating
-              defaultValue={list.starRating}
-              precision={0.5}
-              readOnly
-              sx={{
-                color: randomColorWithAccent,
-                transition: "all 300ms ease-in-out",
-              }}
-            />
-          </Box>
-        </Box>
-      </Paper>
-    );
-  }
+  //       <Box
+  //         display={"flex"}
+  //         flexDirection={"column"}
+  //         justifyContent={"space-between"}
+  //         flexGrow={1}
+  //       >
+  //         <Typography
+  //           variant={"body"}
+  //           noWrap={
+  //             matchesMd || (!displayText && specificCardToDisplay === i)
+  //               ? false
+  //               : true
+  //           }
+  //           maxWidth={"65ch"}
+  //         >
+  //           {list.testimonial}
+  //         </Typography>
+  //         <Box
+  //           alignSelf={"flex-end"}
+  //           display={"flex"}
+  //           alignItems={"center"}
+  //           justifyContent={"center"}
+  //         >
+  //           <Typography variant={"caption"}>Rating:</Typography>
+  //           <Rating
+  //             defaultValue={list.starRating}
+  //             precision={0.5}
+  //             readOnly
+  //             sx={{
+  //               color: randomColorWithAccent,
+  //               transition: "all 300ms ease-in-out",
+  //             }}
+  //           />
+  //         </Box>
+  //       </Box>
+  //     </Paper>
+  //   );
+  // }
 
   function filterTestimonials(list, i) {
     if (page === 1)
@@ -240,64 +255,67 @@ export default function TestimonialsComp(props) {
         gap={1}
         flexDirection={!matchesMd ? "column" : "row"}
       >
-        {test.map((list, i) => (
-          <Grow
-            key={i}
-            in={checked}
-            style={{ transformOrigin: "0 0 0" }}
-            {...(checked ? { timeout: 1000 * i } : {})}
-          >
-            <Paper
-              // onClick={() => {
-              //   console.log(i);
-              //   setDisplayText(!displayText);
-              // }}
-              onClick={handleChange(i)}
-              sx={{
-                display: "flex",
-                flex: 1,
-                flexDirection: "column",
-                padding: 1,
-                gap: 1,
-                width: `${!matchesSm ? "82.5vw" : null}`,
-              }}
+        {test.map((list, i) =>
+          !matchesMd ? (
+            <Accordion
+              key={i}
+              onChange={handleExpandedChange(`panel${i}`)}
+              expanded={expanded === `panel${i}`}
             >
-              <Box
-                display={"flex"}
-                justifyContent={"center"}
-                gap={1}
-                alignItems={"center"}
+              <AccordionSummary
+                expandIcon={<ExpandMore />}
+                aria-controls={`panel${i}bh-content`}
+                id={`panel${i}bh-header`}
               >
-                <Avatar {...stringAvatar(`${list.name}`)} />
-                <Typography
-                  // variant={'h3'}
-                  fontFamily={"Share Tech Mono"}
-                  component={"h3"}
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    // maxWidth: "90vw",
+                  }}
                 >
-                  {list.name}
-                </Typography>
-              </Box>
+                  <Typography
+                    variant={"h3"}
+                    color={"info.main"}
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "flex-start",
+                      gap: 1,
+                    }}
+                  >
+                    <Avatar {...stringAvatar(`${list.name}`)} />
+                    {list.name}
+                  </Typography>
+
+                  {!matchesMd && expandedPanel != `panel${i}` && (
+                    <Typography
+                      variant={"body2"}
+                      // textAlign={"center"}
+                      // gutterBottom
+                      maxWidth={"62ch"}
+                      // width={'100%'}
+                      noWrap={matchesMd ? false : true}
+                      sx={{ color: "text.secondary" }}
+                    >
+                      {list.testimonial}
+                    </Typography>
+                  )}
+                </Box>
+              </AccordionSummary>
+
               <Skelebar h={2} w={"100%"} my={0} clr={randomColorWithAccent} />
 
-              <Box
-                display={"flex"}
-                flexDirection={"column"}
-                justifyContent={"space-between"}
-                flexGrow={1}
-              >
+              <AccordionDetails>
                 <Typography
                   variant={"body2"}
                   textAlign={"center"}
                   gutterBottom
-                  noWrap={
-                    matchesMd || (!displayText && specificCardToDisplay === i)
-                      ? false
-                      : true
-                  }
                   maxWidth={"65ch"}
                 >
                   {list.testimonial}
                 </Typography>
+
                 <Box
                   alignSelf={"flex-end"}
                   display={"flex"}
@@ -315,10 +333,88 @@ export default function TestimonialsComp(props) {
                     }}
                   />
                 </Box>
-              </Box>
-            </Paper>
-          </Grow>
-        ))}
+              </AccordionDetails>
+            </Accordion>
+          ) : (
+            <Grow
+              key={i}
+              in={checked}
+              style={{ transformOrigin: "0 0 0" }}
+              {...(checked ? { timeout: 1000 * i } : {})}
+            >
+              <Paper
+                // onClick={() => {
+                //   console.log(i);
+                //   setDisplayText(!displayText);
+                // }}
+                // onClick={handleChange(i)}
+                sx={{
+                  display: "flex",
+                  flex: 1,
+                  flexDirection: "column",
+                  padding: 1,
+                  gap: 1,
+                  width: `${!matchesSm ? "82.5vw" : null}`,
+                }}
+              >
+                <Box
+                  display={"flex"}
+                  justifyContent={"center"}
+                  gap={1}
+                  alignItems={"center"}
+                >
+                  <Avatar {...stringAvatar(`${list.name}`)} />
+                  <Typography
+                    // variant={'h3'}
+                    fontFamily={"Share Tech Mono"}
+                    component={"h3"}
+                  >
+                    {list.name}
+                  </Typography>
+                </Box>
+                <Skelebar h={2} w={"100%"} my={0} clr={randomColorWithAccent} />
+
+                <Box
+                  display={"flex"}
+                  flexDirection={"column"}
+                  justifyContent={"space-between"}
+                  flexGrow={1}
+                >
+                  <Typography
+                    variant={"body2"}
+                    textAlign={"center"}
+                    gutterBottom
+                    noWrap={
+                      matchesMd || (!displayText && specificCardToDisplay === i)
+                        ? false
+                        : true
+                    }
+                    maxWidth={"65ch"}
+                  >
+                    {list.testimonial}
+                  </Typography>
+                  <Box
+                    alignSelf={"flex-end"}
+                    display={"flex"}
+                    alignItems={"center"}
+                    justifyContent={"center"}
+                  >
+                    <Typography variant={"caption"}>Rating:</Typography>
+                    <Rating
+                      defaultValue={list.starRating}
+                      precision={0.5}
+                      readOnly
+                      sx={{
+                        color: randomColorWithAccent,
+                        transition: "all 300ms ease-in-out",
+                      }}
+                    />
+                  </Box>
+                </Box>
+              </Paper>
+            </Grow>
+          )
+        )}
       </Box>
 
       <Pagination
